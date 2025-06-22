191cb786bbb1d2a1d8bbfa596705520a7d21e230
balazshappygastro <farkasbalazs4774@gmail.com>
2025-06-22 20:34:36 +0200

    navbar renderer

--- apps/renderer/src/app/components/Navbar.tsx
+++ apps/renderer/src/app/components/Navbar.tsx
@@ -1,17 +1,21 @@
 'use client'
 
 import { useCompanyStore } from '@/store/useCompanyStore'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Navbar() {
   const { companyData } = useCompanyStore()
-  const navLinks = companyData?.pageSections?.['navbar']?.content?.links
+  const navbar = companyData?.pageSections?.['navbar']
+  const logo = navbar?.content?.logo
+  const navLinks = navbar?.content?.links
 
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
-        <Link href="/" className="font-extrabold text-xl">
-          WebForger
+        <Link href="/" className="font-extrabold text-xl flex flex-row items-center gap-2" >
+          {logo && <Image src={logo} alt={`${companyData?.name} logo`}  width={60} height={60} className='rounded-full'/>}
+          {companyData?.name}
         </Link>
       </div>
       <div className="flex-none">
